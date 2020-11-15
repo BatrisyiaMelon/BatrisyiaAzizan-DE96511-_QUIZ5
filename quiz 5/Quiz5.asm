fd3a3af459016fae49ec4caf04659e09356f98c2
BatrisyiaMelon <74464803+BatrisyiaMelon@users.noreply.github.com>
2020-11-15 21:56:43 +0800

    MakeupQuiz5

---
+++ quiz 5/Quiz5.asm
@@ -0,0 +1,19 @@
+		 	#include<p18F4550.inc>	
+
+			org 0x00
+			goto	start
+			org 0x08
+			retfie
+			org 0x18
+			retfie
+			
+start		MOVLW 0x01
+			MOVWF 0x07,A
+			MOVWF 0x06,A
+			MOVWF 0x05,A
+			MOVWF 0x04,A
+			MOVWF 0x03,A
+			MOVWF 0x02,A
+			MOVWF 0x01,A
+			MOVWF 0x00,A
+			END
